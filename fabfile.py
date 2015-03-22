40ad5111e76dd51666399af37b1ecb161783cdd8
Mandla Mbuli <lm.mbuli@gmail.com>
2015-03-22 18:10:01 +0200

    Using config.ini file for credentials and directories

--- fabfile.py
+++ fabfile.py
@@ -1,56 +1,70 @@
 '''
+    Deploying to mandla.me via FTP
 '''
+from __future__ import print_function
 from fabric.api import task
 from fabric.colors import red, green, yellow, blue
 
 from ftplib import FTP
 
 import os
+from ConfigParser import SafeConfigParser
 
 
-FTP_ADDRESS = ""
-FTP_USER = ""
-FTP_PASSWORD = ""
+def get_config(default_path='config.ini'):
+    '''Setup application configuration '''
+    config = SafeConfigParser()
+    path = default_path
+    if os.path.exists(path):
+        with open(path) as source:
+            config.readfp(source)
+    else:
+        error_msg = "{} does not exist".format(path)
+        print(red(error_msg))
+        raise IOError(error_msg)
+    return config
 
-INDEX_FTP_DIR = "/public_html"
-BLOG_FTP_DIR = "/public_html/blog"
-INDEX_LOCAL_DIR = "/home/leny/Projects/mandla.me/index"
-BLOG_LOCAL_DIR = "/home/leny/Projects/mandla.me/blog"
 
-
-def connect(host=FTP_ADDRESS, username=FTP_USER, passwd=FTP_PASSWORD):
+def connect(config):
     '''Connects to the FTP server'''
+    host = config.get("credentials", "host")
+    username = config.get("credentials", "username")
+    password = config.get("credentials", "password")
+
     ftp = FTP(host=host)
-    ftp.login(user=username, passwd=passwd)
+    ftp.login(user=username, passwd=password)
 
     return ftp
 
 
 @task
 def test_ftp():
-    ftp = connect()
-    print blue(FTP.getwelcome(ftp))
+    config = get_config()
+    ftp = connect(config)
+    print(blue(FTP.getwelcome(ftp)))
     ftp.quit()
 
 
 @task
 def deploy():
-    print ""
-    print green("** Upload latest changes **")
+    config = get_config()
 
-    ftp = connect()
-    f = os.path.join(INDEX_LOCAL_DIR, "test.txt")
+    ftp = connect(config)
+    remote_root = config.get("directories", "remote_root")
+    local_root = config.get("directories", "local_root")
+
+    ftp.cwd(remote_root)
+    f = os.path.join(local_root, "index", "test.txt")
     split = os.path.split(f)
-    remote_dir = INDEX_FTP_DIR
-    ftp.cwd(remote_dir)
 
-    print blue('STOR %s' % f) , blue(" -> "), os.path.join(remote_dir, split[1]),
+    print(blue('STOR %s' % f), end="")
+    print(blue(" -> "), end="")
+    print(os.path.join(remote_root, split[1]), end="")
+
     try:
        ftp.storlines("STOR %s" % split[1], open(f, "r"))
-       print green(u'\u2713')
+       print(green(u' \u2713'))
     except IOError as e:
-       print red('>> %s' % e.strerror)
+       print(red('>> %s' % e.strerror))
 
-    print ""
-    print green('** Closing FTP connection **')
     ftp.quit()
